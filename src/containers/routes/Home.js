06b6053aa74e71c1af04be4a3fab11764328bc8a
Eric <ericzpy1998@outlook.com>
2020-01-04 17:53:12 -0500

    adding banner to home page

--- src/containers/routes/Home.js
+++ src/containers/routes/Home.js
@@ -3,16 +3,22 @@ import {NavLink, withRouter} from "react-router-dom";
 import Text from "../../components/accessories/Text";
 import XHelmet from "../../components/accessories/XHelmet";
 import {logo} from "../../assets/exportImages";
+import './Home.css';
 
 class Home extends Component{
     render() {
         return(
             <Fragment>
-                <XHelmet title={"iQueensU"} favicon={logo}/>
-                <h3><Text id={"home"}/></h3>
-                <NavLink to={"/qhousing"}>Go Qhousing</NavLink><br/>
-                <NavLink to={"/qucumber"}>Go Qucumber</NavLink><br/>
-                <br/>
+                <div className="InternationalHeader">
+                    {/* change h1 to nav items */}
+                    <h1 className="testHeader" >
+                        <XHelmet title={"iQueensU"} favicon={logo}/>
+                        <h3><Text id={"home"}/></h3>
+                        <NavLink to={"/qhousing"}>Go Qhousing</NavLink><br/>
+                        <NavLink to={"/qucumber"}>Go Qucumber</NavLink><br/>
+                        <br/>
+                    </h1>
+                </div>
             </Fragment>
         )
     }
