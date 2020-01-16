9bb8754799300ec5cc603bcf9a3cdc9c6cad4395
Eric <ericzpy1998@outlook.com>
2020-01-15 23:48:05 -0500

    merging conflicts

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,24 +2,11 @@
 import React, {Component}                    from 'react';
 import {connect}                             from "react-redux";
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
-<<<<<<< HEAD
-import Navigation, {NavItem} from "../components/layouts/Navigation"
-import Footer from "../components/layouts/Footer";
-// Pages
-import Home from "./routes/Home";
-import NotFound from "./routes/NotFound";
-import Qhousing from "./routes/qhousing/Qhousing";
-import Qucumber from "./routes/qucumber/Qucumber";
-import Database from "./routes/qucumber/Database";
-import CoursePage from './routes/qucumber/course/CoursePage';
-
-=======
 //
 import Navigation, {NavItem}                 from "../components/layouts/Navigation";
 import actions                               from "../store/actions/root";
 import history                               from "../utils/history";
 import Footer                                from "../components/layouts/Footer";
->>>>>>> master
 // Styles
 import styles                                from './App.module.css';
 // Pages
@@ -31,38 +18,6 @@ import Database                              from "./routes/qucumber/Database";
 import Qucumber                              from "./routes/qucumber/Qucumber";
 
 class App extends Component {
-<<<<<<< HEAD
-  changeLanguage() {
-    let lang = this.props.locale;
-    lang = lang === 'zh' ? 'en' : 'zh';
-    this.props.changeLanguage(lang);
-  }
-  render() {
-    const { locale } = this.props;
-    return (
-      <div className="App">
-        <main className={wrapper.main}>
-        {/* remove later */}
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/404" exact component={NotFound} />
-            <Route path="/qhousing" exact component={Qhousing} />
-            <Route path="/qucumber" exact component={Qucumber} />
-            <Route path={"/database"} exact component={Database}/>
-            <Route path={"/database/course"} exact component={CoursePage}/>
-            <Redirect from="*" to="/404" />
-          </Switch>
-        </main>
-        {/* <Navigation>
-          <NavItem event={() => {history.push("/")}} name={"Home"} />
-          <NavItem event={() => {history.goBack()}} name={"Back"}/>
-          <NavItem event={() => this.changeLanguage()} name={locale === 'zh' ? '英文' : 'Chinese'}/>
-        </Navigation> */}
-        <Footer />
-      </div>
-    );
-  }
-=======
     constructor(props) {
         super(props);
         this.changeLanguage = this.changeLanguage.bind(this);
@@ -103,7 +58,6 @@ class App extends Component {
             </div>
         );
     }
->>>>>>> master
 }
 
 const mapStateToProps = (state) => ({
--- src/containers/routes/qucumber/course/CourseWrapper.js
+++ src/containers/routes/qucumber/course/CourseWrapper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { connect } from "react-redux";
 import { getCourse } from "../../../../store/actions/courseActions";
@@ -124,47 +123,6 @@ class CourseWrapper extends React.Component{
                         {/* temp */}
                         {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}
                     </div> 
-=======
-// Packages
-import React       from "react";
-import {connect}   from "react-redux";
-//
-import {getCourse} from "../../../../store/actions/courseActions";
-// Styles
-import styles      from './CourseWrapper.module.css';
-
-class CourseWrapper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.props.getCourseTest("noUse");
-    }
-
-    render() {
-        return (
-            <div className={styles.CoursePageFlewContainer}>
-                {/* <Fragment> */}
-                <div className={styles.CoursePageLeftPart}>
-                    {/* temp */}
-                    {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}
-                </div>
-
-                <div className={styles.CoursePageCenterPart}>
-                    {/* <backtrace /> */}
-                    <h1 className={styles.CoursePageCourseSubjAndNum}>
-                        {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["subject"]
-                            + " " + this.props.currentCourse[0]["number"]}
-                    </h1>
-                    <h1 className={styles.CoursePageCourseSubjAndNum}>
-                        {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["subject"]
-                            + " " + this.props.currentCourse[0]["number"]}
-                    </h1>
-                </div>
-
-                <div className={styles.CoursePageRightPart}>
-                    {/* temp */}
-                    {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}
-                </div>
->>>>>>> master
                 {/* </Fragment> */}
             </div>
         );
