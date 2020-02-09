423f210a6078978407bb51653080261c2bc8ff70
Eric <ericzpy1998@outlook.com>
2020-02-08 22:19:17 -0500

    resolved lost focus issue

--- src/containers/routes/login/LoginPage.js
+++ src/containers/routes/login/LoginPage.js
@@ -12,18 +12,13 @@ class LoginPage extends Component {
             error: '',
             regist: false,
         };
-
-        this.handlePassChange = this.handlePassChange.bind(this);
-        this.handleUserChange = this.handleUserChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.dismissError = this.dismissError.bind(this);
     }
 
-    dismissError() {
+    dismissError = () => {
         this.setState({error: ''});
     }
 
-    handleSubmit(evt) {
+    handleSubmit = (evt) => {
         evt.preventDefault();
 
         if (!this.state.username) {
@@ -37,84 +32,82 @@ class LoginPage extends Component {
         return this.setState({error: ''});
     }
 
-    handleUserChange(evt) {
+    handleUserChange = (evt) => {
         this.setState({
             username: evt.target.value,
         });
     }
 
-    handlePassChange(evt) {
+    handlePassChange = (evt) => {
         this.setState({
             password: evt.target.value,
         });
     }
 
+     LoginLayout = (prop) => {
+      return (
+          <div className={styles.login}>
+              <div className={styles.form}>
+                  <form className={prop.type} onSubmit={this.handleSubmit}>
+                      {
+                          this.state.error &&
+                          <h3 data-test="error" onClick={this.dismissError}>
+                              <button onClick={this.dismissError}>✖</button>
+                              {this.state.error}
+                          </h3>
+                      }
+                      {prop.children}
+                  </form>
+              </div>
+          </div>
+      );
+  };
+
+   Reg = (prop) => {
+      return (
+          <>
+              <div className={styles.headtop}>
+                <img className={styles.img} src={Logo} alt={"Logo"}/>
+                <label >Register</label>
+              </div>
+              <input type="text" data-test="username" placeholder="Email" value={this.state.username}
+                     onChange={this.handleUserChange}/>
+              <input type="password" data-test="password" placeholder="Password" value={this.state.password}
+                     onChange={this.handlePassChange}/>
+              <div className={styles.func}>
+                  <a className={styles.change} href="#" onClick={() => {this.setState((state, prop) => ({regist: !state.regist}));}}>Sign in</a>
+                  <button className={styles.but1} type="submit" data-test="submit">Next</button>
+              </div>
+          </>
+      );
+  };
+
+   Login = (prop) => {
+      return (
+          <>
+              <div className={styles.headtop}>
+                <img className={styles.img} src={Logo} alt={"Logo"}/>
+                <label >Login</label>
+              </div>
+              <input id={"username"} type="text" placeholder="Email" value={this.state.username}
+                     onChange={this.handleUserChange}/>
+              <input id={"password"} type="password"  placeholder="Password" value={this.state.password}
+                     onChange={this.handlePassChange}/>
+              <div className={styles.func}>
+                  <a className={styles.change} href="#" onClick={() => {this.setState((state, prop) => ({regist: !state.regist}));}}>Register</a>
+                  <button className={styles.but1} type="submit" data-test="submit">Login</button>
+              </div>
+          </>
+      );
+  };
+
+   //layoutType = this.state.regist ? styles.submit : styles.loginform;
 
     render() {
-        const LoginLayout = (prop) => {
-            return (
-                <div className={styles.login}>
-                    <div className={styles.form}>
-                        <form className={prop.type} onSubmit={this.handleSubmit}>
-                            {
-                                this.state.error &&
-                                <h3 data-test="error" onClick={this.dismissError}>
-                                    <button onClick={this.dismissError}>✖</button>
-                                    {this.state.error}
-                                </h3>
-                            }
-                            {prop.children}
-                        </form>
-                    </div>
-                </div>
-            );
-        };
-
-        const Reg = (prop) => {
-            return (
-                <>
-                    <div className={styles.headtop}>
-                      <img className={styles.img} src={Logo} alt={"Logo"}/>
-                      <label >Register</label>
-                    </div>
-                    <input type="text" data-test="username" placeholder="Email" value={this.state.username}
-                           onChange={this.handleUserChange}/>
-                    <input type="password" data-test="password" placeholder="Password" value={this.state.password}
-                           onChange={this.handlePassChange}/>
-                    <div className={styles.func}>
-                        <a className={styles.change} href="#" onClick={() => {this.setState((state, prop) => ({regist: !state.regist}));}}>Sign in</a>
-                        <button className={styles.but1} type="submit" data-test="submit">Next</button>
-                    </div>
-                </>
-            );
-        };
-
-        const Login = (prop) => {
-            return (
-                <>
-                    <div className={styles.headtop}>
-                      <img className={styles.img} src={Logo} alt={"Logo"}/>
-                      <label >Login</label>
-                    </div>
-                    <input type="text" data-test="username" placeholder="Email" value={this.state.username}
-                           onChange={this.handleUserChange}/>
-                    <input type="password" data-test="password" placeholder="Password" value={this.state.password}
-                           onChange={this.handlePassChange}/>
-                    <div className={styles.func}>
-                        <a className={styles.change} href="#" onClick={() => {this.setState((state, prop) => ({regist: !state.regist}));}}>Register</a>
-                        <button className={styles.but1} type="submit" data-test="submit">Login</button>
-                    </div>
-                </>
-            );
-        };
-
-        const layoutType = this.state.regist ? styles.submit : styles.loginform;
-
-
         return (
-            <LoginLayout type={layoutType}>
-                {this.state.regist ? <Reg/> : <Login/>}
-            </LoginLayout>
+            <this.LoginLayout type={this.layoutType}>
+                {this.state.regist ? <this.Reg/> : <this.Login/>}
+            </this.LoginLayout>
         );
     }
 }
