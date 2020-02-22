f1e3cac2775de778b6d1a1dcd9ba5875f55f825b
Somiona <somionat@gmail.com>
2020-02-22 04:41:55 -0500

    憨八哥儿 done

--- src/components/common/headerBar/HeaderBar.js
+++ src/components/common/headerBar/HeaderBar.js
@@ -1,48 +1,125 @@
 // Packages
-import React, {Fragment}     from "react";
-import {NavLink, withRouter} from "react-router-dom";
+import React, { Fragment } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
 //
-import qcumberIcon          from "../../../assets/images/logos/qcumber.svg";
-import Text                  from "../../accessoriesDeprecated/Text";
+import qcumberIcon from "../../../assets/images/logos/qcumber.svg";
+import Text from "../../accessoriesDeprecated/Text";
 // Styles
-import styles                from './HeaderBar.module.css';
-import scssStyles            from './HeaderBar.module.scss';
+import Styles from "./HeaderBar.module.scss";
+import "./_burger.scss";
+import BurerItemStyles from "./WithBurger.module.scss";
 // props:
 // hasRightAlignedItems (Optional): if containing any navlink that aligns to right
 
 class HeaderBar extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { useBurger: false };
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+
+    resize() {
+        this.setState({ useBurger: window.innerWidth <= 1020 });
+    }
+
+    Header(props) {
+        return <div className={Styles.QHeader}>{props.children}</div>;
+    }
+
+    Hamburger(props) {
+        return <Menu>{props.children}</Menu>;
+    }
+
+    WithBurger(props) {
+        const Burger = this.Hamburger;
         return (
-            <Fragment>
-                <div className={styles.QHeader}>
-                    {/* change h1 to nav items */}
-                    <div className={styles.QNavBar}>
-                        <div className={scssStyles.QNavBarList}>
-                            {/* change css to design in left card.css */}
-                            <NavLink to={"/qcumber"} className={styles.QLogo}>
-                                <img width={28.2} height={28.2} src={qcumberIcon} alt={"qcumberIcon"}/>
-                            </NavLink>
+            <Burger>
+                <div className={BurerItemStyles.QNavBarLink}>
+                    {/* change css to design in left card.css */}
+                    <NavLink to={"/qcumber"} className={Styles.QLogo}>
+                        <img width={28.2} height={28.2} src={qcumberIcon} alt={"qcumberIcon"} />
+                    </NavLink>
 
-                            {/* or refactor into a separate component */}
-                            <NavLink to={"/qcumber"} className={scssStyles.Qcumber} activeClassName="chosen" >Qcumber</NavLink>
-
-                            <NavLink to={"/"} activeClassName="chosen"><Text id={"home"}/></NavLink>
-                            <NavLink to={"/database"} activeClassName="chosen">Database</NavLink>
-                            <NavLink to={"/about"} activeClassName="chosen"><Text id={"about"}/></NavLink>
-                        </div>
-                    </div>
-                    {this.props.hasRightAlignedItems ?
-                        <div className={scssStyles.QNavBarRight}>
-                            <div className={scssStyles.SignUp}>
-                                <NavLink to={"/about"}>Sign Up</NavLink>
-                            </div>
-                            <NavLink to={"/about"}>Sign In</NavLink>
-                        </div> : null}
+                    {/* or refactor into a separate component */}
+                    <NavLink to={"/qcumber"} className={Styles.Qcumber} activeClassName="chosen">
+                        Qcumber
+                    </NavLink>
                 </div>
-            </Fragment>
+
+                <NavLink to={"/"} className={BurerItemStyles.QNavBarLink} activeClassName="chosen">
+                    <Text id={"home"} />
+                </NavLink>
+                <NavLink to={"/database"} className={BurerItemStyles.QNavBarLink} activeClassName="chosen">
+                    Database
+                </NavLink>
+                <NavLink to={"/about"} className={BurerItemStyles.QNavBarLink} activeClassName="chosen">
+                    <Text id={"about"} />
+                </NavLink>
+
+                {this.props.hasRightAlignedItems ? (
+                    <Fragment>
+                        <hr className={BurerItemStyles.QSeprator}/>
+                        <div className={`${BurerItemStyles.QNavBarLink} ${Styles.SignUp}`}>
+                            <NavLink to={"/about"}>Sign Up</NavLink>
+                        </div>
+                        <NavLink className={BurerItemStyles.QNavBarLink} to={"/about"}>
+                            Sign In
+                        </NavLink>
+                    </Fragment>
+                ) : null}
+            </Burger>
         );
     }
 
+    WithoutBurger(props) {
+        return (
+            <div className={Styles.QHeader}>
+                {/* change h1 to nav items */}
+                <div className={Styles.QNavBar}>
+                    <div className={Styles.QNavBarList}>
+                        {/* change css to design in left card.css */}
+                        <NavLink to={"/qcumber"} className={Styles.QLogo}>
+                            <img width={28.2} height={28.2} src={qcumberIcon} alt={"qcumberIcon"} />
+                        </NavLink>
+
+                        {/* or refactor into a separate component */}
+                        <NavLink to={"/qcumber"} className={Styles.Qcumber} activeClassName="chosen">
+                            Qcumber
+                        </NavLink>
+
+                        <NavLink to={"/"} activeClassName="chosen">
+                            <Text id={"home"} />
+                        </NavLink>
+                        <NavLink to={"/database"} activeClassName="chosen">
+                            Database
+                        </NavLink>
+                        <NavLink to={"/about"} activeClassName="chosen">
+                            <Text id={"about"} />
+                        </NavLink>
+                    </div>
+                </div>
+                {this.props.hasRightAlignedItems ? (
+                    <div className={Styles.QNavBarRight}>
+                        <div className={Styles.SignUp}>
+                            <NavLink to={"/about"}>Sign Up</NavLink>
+                        </div>
+                        <NavLink to={"/about"}>Sign In</NavLink>
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
+
+    render() {
+        const Content = this.state.useBurger ? this.WithBurger.bind(this) : this.WithoutBurger.bind(this);
+
+        return <Content />;
+    }
 }
 
 export default withRouter(HeaderBar);
