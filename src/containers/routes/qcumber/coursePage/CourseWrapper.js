1fb532c5be1bfd6c4367b573512ff2d879abe7b3
pipiFox <xinyuhu0511@gmail.com>
2020-04-16 01:49:19 -0400

    workload finished

--- src/containers/routes/qcumber/coursePage/CourseWrapper.js
+++ src/containers/routes/qcumber/coursePage/CourseWrapper.js
@@ -14,8 +14,8 @@ class CourseWrapper extends React.Component {
         };
     }
 
-    componentDidMount(){
-        this.props.getCourseTest("noUse"); 
+    componentDidMount() {
+        this.props.getCourseTest("noUse");
         this.makeApiCall("CISC199");
     }
 
@@ -36,8 +36,8 @@ class CourseWrapper extends React.Component {
     render() {
         console.log("render", this.state.courseDetails);
         let courseLoadTitle = "";
-        if(typeof (this.state.courseDetails) !== "undefined"){
-            if(this.state.courseDetails ['course_details'] ["learning_hours"] !=="-1"){
+        if (typeof (this.state.courseDetails) !== "undefined") {
+            if (this.state.courseDetails ['course_details']["learning_hours"] !== "-1") {
                 courseLoadTitle = "Course Load";
             }
         }
@@ -52,116 +52,121 @@ class CourseWrapper extends React.Component {
                 </div>
 
                 <div className={styles.coursePageCenterPart}>
-                        {/* <backtrace /> */}
-                        <div>
-                            <h1 className={styles.coursePageCourseSubjAndNum}>
-                                {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["subject"]
+                    {/* <backtrace /> */}
+                    <div>
+                        <h1 className={styles.coursePageCourseSubjAndNum}>
+                            {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["subject"]
                                 + " " + this.props.currentCourse[0]["number"]}
-                            </h1>
+                        </h1>
+                    </div>
+
+                    {/* small table containing Career, Units, Academic Group, Academic Organization */}
+                    <div className={styles.mytable}>
+                        <div className={styles.column1}></div>
+                        <div className={styles.column2}></div>
+                        <div className={styles.column3}></div>
+                        <div className={styles.column4}></div>
+                        <div className={styles.myrow}>
+                            <div className={styles.mycellHeader}>Career</div>
+                            <div
+                                className={styles.mycell}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["career"]}</div>
+                            <div className={styles.mycellHeader}>Academic Group</div>
+                            <div
+                                className={styles.mycell}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["academic_group"]}</div>
                         </div>
-
-                        {/* small table containing Career, Units, Academic Group, Academic Organization */}
-                        <div className={styles.mytable}>
-                            <div className={styles.column1}></div>
-                            <div className={styles.column2}></div>
-                            <div className={styles.column3}></div>
-                            <div className={styles.column4}></div>
-                            <div className={styles.myrow}>
-                                <div className={styles.mycellHeader}>Career</div>
-                                <div className={styles.mycell}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["career"]}</div>
-                                <div className={styles.mycellHeader}>Academic Group</div>
-                                <div className={styles.mycell} >{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["academic_group"]}</div>
-                            </div>
-                            <div className={styles.myrow}>
-                                <div className={styles.mycellHeader}>Units</div>
-                                <div className={styles.mycell} style={{color:"green"}}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}</div>
-                                <div className={styles.mycellHeader}>Academic Organization</div>
-                                <div className={styles.mycell}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["academic_organization"]}</div>
-                            </div>
-                            
-                        </div>
-
-                        <div className={styles.mytable} style={{marginTop:"3.5rem"}}>
-                            <div className={styles.column1}></div>
-                            <div className={styles.column5}></div>
-                            <div className={styles.myrow}>
-                                <div className={styles.mycellHeader}>Description</div>
-                                <div className={styles.mycellWholeRight}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["course_description"]}</div>
-                            </div>
-                        </div>
-
-                        {/* 有另外的api calls 来接收页面上的courseObj的前提条件们 */}
-                        <div className={styles.mytable} style={{marginTop:"1.5rem"}}>
-                            <div className={styles.column1}></div>
-                            <div className={styles.myrow}>
-                                <div className={styles.mycellHeader}>Prerequisite</div>
-                            </div>
-
-                            <div className={styles.myrow}>
-                                {/* Prerequisite bars */}
-                                <div className={styles.mycellWholeRight}>
-                                    {/* a must have: the level bar */}
-                                    <StatusBar
-                                        prerequisiteItem={"Level of study: 2 or above"}
-                                        userGrade={"Level 2"}
-                                        valid={"valid"} />
-                                    
-                                    {/* optional using .map particular course */}
-                                    {/* green */}
-                                    <StatusBar
-                                        prerequisiteItem={"C- in CISC203"}
-                                        userGrade={"2018 Fall A+"}
-                                        valid={"valid"}/>
-
-                                    {/* red */}
-                                    <StatusBar
-                                        prerequisiteItem={"C- in CISC124"}
-                                        userGrade={"Not Taken Yet"}
-                                        valid={"nonValid"} />
-
-                                    {/* 按钮和那个dropdown */}
-
-                                </div>
-                            </div>
-                                {/* <div className={styles.mycellWholeRight}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["course_description"]}</div> */}
-                        </div>
-
-                        {/* getCourse也有另外的api(), 所以也可能isolate*/}
-                        {/* Course Load Part */}
-                        <div className={styles.mytable} style={{marginTop:"2rem"}}>
-                            <div className="column6"></div>
-                            <div className={styles.column5}></div>
-                            <div className={styles.myrow}>
-                                <div className={styles.mycellHeader}>
-                                    {courseLoadTitle}
-                                </div>
-                            </div>
-
-                            <div className={styles.myrow}>
-                                {/* designed Component */}
-
-                                <CourseLoadBar
-                                    workLoad={typeof (this.state.courseDetails) !== "undefined" ? this.state.courseDetails ['course_details'] ["learning_hours"] : undefined}
-                                        // mock data, need to be deleted
-                                    tableHeaders = {["Lecture", "Tutorial", "Lab", "Practice", "Total", "Load"]}
-                                    lecture={"3 h/week"}
-                                    tutorial={"-"}
-                                    lab={"1 h/week"}
-                                    practice={"6 h/week"}
-                                    total={"10 h/week"}
-                                    totalPerTerm={"120 h/term"}
-                                    load={"Regular"} />
-                               
-                                
-                            </div>
+                        <div className={styles.myrow}>
+                            <div className={styles.mycellHeader}>Units</div>
+                            <div className={styles.mycell}
+                                 style={{color: "green"}}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}</div>
+                            <div className={styles.mycellHeader}>Academic Organization</div>
+                            <div
+                                className={styles.mycell}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["academic_organization"]}</div>
                         </div>
 
                     </div>
 
-                    <div className={styles.coursePageRightPart}>
-                        {/* temp */}
-                        {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}
-                    </div> 
+                    <div className={styles.mytable} style={{marginTop: "3.5rem"}}>
+                        <div className={styles.column1}></div>
+                        <div className={styles.column5}></div>
+                        <div className={styles.myrow}>
+                            <div className={styles.mycellHeader}>Description</div>
+                            <div
+                                className={styles.mycellWholeRight}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["course_description"]}</div>
+                        </div>
+                    </div>
+
+                    {/* 有另外的api calls 来接收页面上的courseObj的前提条件们 */}
+                    <div className={styles.mytable} style={{marginTop: "1.5rem"}}>
+                        <div className={styles.column1}></div>
+                        <div className={styles.myrow}>
+                            <div className={styles.mycellHeader}>Prerequisite</div>
+                        </div>
+
+                        <div className={styles.myrow}>
+                            {/* Prerequisite bars */}
+                            <div className={styles.mycellWholeRight}>
+                                {/* a must have: the level bar */}
+                                <StatusBar
+                                    prerequisiteItem={"Level of study: 2 or above"}
+                                    userGrade={"Level 2"}
+                                    valid={"valid"}/>
+
+                                {/* optional using .map particular course */}
+                                {/* green */}
+                                <StatusBar
+                                    prerequisiteItem={"C- in CISC203"}
+                                    userGrade={"2018 Fall A+"}
+                                    valid={"valid"}/>
+
+                                {/* red */}
+                                <StatusBar
+                                    prerequisiteItem={"C- in CISC124"}
+                                    userGrade={"Not Taken Yet"}
+                                    valid={"nonValid"}/>
+
+                                {/* 按钮和那个dropdown */}
+
+                            </div>
+                        </div>
+                        {/* <div className={styles.mycellWholeRight}>{this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["course_description"]}</div> */}
+                    </div>
+
+                    {/* getCourse也有另外的api(), 所以也可能isolate*/}
+                    {/* Course Load Part */}
+                    <div className={styles.mytable} style={{marginTop: "2rem"}}>
+                        <div className="column6"></div>
+                        <div className={styles.column5}></div>
+                        <div className={styles.myrow}>
+                            <div className={styles.mycellHeader}>
+                                {courseLoadTitle}
+                            </div>
+                        </div>
+
+                        <div className={styles.myrow}>
+                            {/* designed Component */}
+
+                            <CourseLoadBar
+                                workLoad={typeof (this.state.courseDetails) !== "undefined" ? this.state.courseDetails ['course_details']["learning_hours"] : undefined}
+                                // mock data, need to be deleted
+                                tableHeaders={["Lecture", "Tutorial", "Lab", "Practice", "Total", "Load"]}
+                                lecture={"3 h/week"}
+                                tutorial={"-"}
+                                lab={"1 h/week"}
+                                practice={"6 h/week"}
+                                total={"10 h/week"}
+                                totalPerTerm={"120 h/term"}
+                                load={"Regular"}/>
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className={styles.coursePageRightPart}>
+                    {/* temp */}
+                    {this.props.currentCourse.length < 1 ? null : this.props.currentCourse[0]["units"]}
+                </div>
                 {/* </Fragment> */}
             </div>
         );
@@ -179,7 +184,6 @@ const mapDispatchToProps = dispatch => {
         getCourseTest: noUse => dispatch(getCourse(noUse)),
     };
 };
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(CourseWrapper);
