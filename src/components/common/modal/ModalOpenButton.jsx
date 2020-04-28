6362ae635f445325238fd44a8ecda93747149b5f
Somiona <somionat@gmail.com>
2020-04-28 04:33:44 -0400

    fixed accidentally messed up prop of modal button

--- src/components/common/modal/ModalOpenButton.jsx
+++ src/components/common/modal/ModalOpenButton.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 
 class ModalOpenButton extends Component {
     render() {
-        const { onClick, children, ...rest } = this.props;
-        const Children = children
-        
+        const { modalToggle, onClick, children, ...rest } = this.props;
+        const Children = children;
+
         const handleClick = () => {
-            this.props.modalToggle();
-            this.props.onClick && this.props.onClick();
+            modalToggle();
+            onClick && onClick();
         };
 
         return <Children onClick={handleClick} {...rest} />;
@@ -16,7 +16,7 @@ class ModalOpenButton extends Component {
 }
 
 ModalOpenButton.propTypes = {
-    modalToggle: PropTypes.func.isRequired
+    modalToggle: PropTypes.func.isRequired,
 };
 
 export default ModalOpenButton;
